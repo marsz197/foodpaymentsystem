bc0d59892a3f30372aaad608217c46ab9444095e
Marcus <letantruong197@gmail.com>
2025-02-25 11:24:23 +0700

    add sdk firebase and main js

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCV49Xr9GECNH5O9jWt0Nib4AyWNPxXUkA",
+  authDomain: "food-payment-sys-firebase-app.firebaseapp.com",
+  projectId: "food-payment-sys-firebase-app",
+  storageBucket: "food-payment-sys-firebase-app.firebasestorage.app",
+  messagingSenderId: "1041294912760",
+  appId: "1:1041294912760:web:576bb9348325d840359f0d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
