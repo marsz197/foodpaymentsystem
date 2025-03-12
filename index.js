775c7cb390cbc11bd1c8bb56595f92dc8013ecdc
Marcus <letantruong197@gmail.com>
2025-03-12 12:11:49 +0700

    fixing and working on firestore

--- index.js
+++ index.js
@@ -1,48 +1,50 @@
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
-  import { getAuth,createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/11.3.1/firebase-auth.js'
-  import { getFirestore, collection, addDoc, getDocs, setDoc } from 'https://www.gstatic.com/firebasejs/11.3.1/firebase-firestore.js'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/11.3.1/firebase-auth.js'
+import { getFirestore, collection, addDoc, getDocs, setDoc } from 'https://www.gstatic.com/firebasejs/11.3.1/firebase-firestore.js'
 
-  // password-toggle
-  document.addEventListener('DOMContentLoaded', function() {
-    const passwordButtons = document.getElementsByClassName('toggle-password');
 
-    Array.from(passwordButtons).forEach(button => {
-      button.addEventListener('click', togglePassword);
-    });
+// Load environment variables
+const firebaseConfig = {
+  apiKey: "AIzaSyCV49Xr9GECNH5O9jWt0Nib4AyWNPxXUkA",
+  authDomain: "food-payment-sys-firebase-app.firebaseapp.com",
+  projectId: "food-payment-sys-firebase-app",
+  storageBucket: "food-payment-sys-firebase-app.firebasestorage.app",
+  messagingSenderId: "1041294912760",
+  appId: "1:1041294912760:web:576bb9348325d840359f0d"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
-    function togglePassword() {
-      const passwordField = document.getElementById('password');
-      const eyeIcon = document.getElementById('eye_icon');
+// password-toggle
+document.addEventListener('DOMContentLoaded', function () {
+  const passwordButtons = document.getElementsByClassName('toggle-password');
 
-      if (passwordField.type === 'password') {
-        passwordField.type = 'text';
-        eyeIcon.classList.remove('fa-eye');
-        eyeIcon.classList.add('fa-eye-slash');
-      } else {
-        passwordField.type = 'password';
-        eyeIcon.classList.remove('fa-eye-slash');
-        eyeIcon.classList.add('fa-eye');
-      }
-    }
+  Array.from(passwordButtons).forEach(button => {
+    button.addEventListener('click', togglePassword);
   });
 
-  // Load environment variables
-  const firebaseConfig = {
-    apiKey: "AIzaSyCV49Xr9GECNH5O9jWt0Nib4AyWNPxXUkA",
-    authDomain: "food-payment-sys-firebase-app.firebaseapp.com",
-    projectId: "food-payment-sys-firebase-app",
-    storageBucket: "food-payment-sys-firebase-app.firebasestorage.app",
-    messagingSenderId: "1041294912760",
-    appId: "1:1041294912760:web:576bb9348325d840359f0d"
-  };
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
-  const db = getFirestore(app);
+  function togglePassword() {
+    const passwordField = document.getElementById('password');
+    const eyeIcon = document.getElementById('eye_icon');
 
-  // signup and signin
-  const signupForm = document.getElementById('signup');
+    if (passwordField.type === 'password') {
+      passwordField.type = 'text';
+      eyeIcon.classList.remove('fa-eye');
+      eyeIcon.classList.add('fa-eye-slash');
+    } else {
+      passwordField.type = 'password';
+      eyeIcon.classList.remove('fa-eye-slash');
+      eyeIcon.classList.add('fa-eye');
+    }
+  }
+});
+
+// signup and signin
+const signupForm = document.getElementById('signup');
+if (signupForm) {
   signupForm.addEventListener('submit', async (event) => {
     event.preventDefault(); // Prevent default form submission
 
@@ -55,8 +57,8 @@
       console.log("User created:", user);
       document.getElementById('error-message').innerHTML = "User created successfully !!!"
       document.getElementById('error-message').style.color = "green"
-      
-      const docRef = await addDoc(collection(db, "users"), {
+
+      const docRef = await addDoc(collection(db, "users",user.uid), {
         uid: user.uid,
         user_email: user.email,
         created_at: new Date(),
@@ -64,7 +66,7 @@
       });
       console.log("User saved to Firestore with ID:", docRef.id);
 
-      setTimeout(()=>{window.location.replace("signin.html")},4000)
+      setTimeout(() => { window.location.replace("signin.html") }, 4000)
       // Redirect to a success page or handle user creation successfully.
     } catch (error) {
       const errorCode = error.code;
@@ -73,11 +75,13 @@
       document.getElementById('error-message').innerHTML = errorCode + errorMessage;
     }
   });
+}
 
-  const signinForm = document.getElementById('signin');
+const signinForm = document.getElementById('signin');
+if (signinForm) {
   signinForm.addEventListener('submit', async (event) => {
+    console.log("Submited")
     event.preventDefault();
-
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
@@ -85,6 +89,10 @@
       const userCredential = await signInWithEmailAndPassword(auth, email, password);
       const user = userCredential.user;
       console.log("User signed in:", user);
+      document.getElementById('error-message').innerHTML = "User login successfully !!!"
+      document.getElementById('error-message').style.color = "green"
+      localStorage.setItem("user",JSON.stringify(user.uid))
+      setTimeout(() => { window.location.replace("index.html") }, 4000)
       // Redirect to the app's main content or a user dashboard.
     } catch (error) {
       const errorCode = error.code;
@@ -94,6 +102,23 @@
       // Display an error message to the user.  Common errors include wrong password, etc.
     }
   });
-console.log("Load sucessfully")
-
-
+}
+if(localStorage.getItem("user")){
+  const uid = JSON.parse(localStorage.getItem("user"))
+  getUserData(uid)
+  async function getUserData(uid) {
+    try {
+        const userRef = doc(db, "users", uid);
+        const userSnap = await getDoc(userRef);
+        if (userSnap.exists()) {
+            console.log("User Data:", userSnap.data());
+            return userSnap.data();
+        } else {
+            console.log("No such user found!");
+            return null;
+        }
+    } catch (error) {
+        console.error("Error fetching user data:", error);
+    }
+  }
+}
